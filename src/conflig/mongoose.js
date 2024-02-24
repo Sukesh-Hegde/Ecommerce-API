ec74c6b8f88d0190acf59894aeeb82c3189f8423
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-02-24 14:50:14 +0530

    updated DB

--- src/conflig/mongoose.js
+++ src/conflig/mongoose.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 // import dotenv from "dotenv";
 // dotenv.config();
-
-const URL="mongodb://localhost:27017/Ecom-API"
+// mongodb+srv://sukesh:<password>@cluster0.cvyliz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// const URL="mongodb://localhost:27017/Ecom-API"
+const URL="mongodb+srv://sukesh:1234@cluster0.cvyliz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 export const connectUsingMongoose = async()=>{
     try{
         await mongoose.connect(URL,{
