f47744798cf3b2c6defa4d9a632100223cabbba8
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-05-06 11:53:44 +0530

    imported dotenv

--- src/conflig/mongoose.js
+++ src/conflig/mongoose.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
+
 // mongodb+srv://sukesh:<password>@cluster0.cvyliz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 // const URL="mongodb://localhost:27017/Ecom-API"
 const URL="mongodb+srv://sukesh:1234@cluster0.cvyliz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
